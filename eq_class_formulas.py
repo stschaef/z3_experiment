a15b5e5105deea7ec9ddecc946991e4ab2b6dc56
Steven <stschaef@umich.edu>
2022-03-30 23:07:58 -0400

    can get counts for one eq class

--- eq_class_formulas.py
+++ eq_class_formulas.py
@@ -1,4 +1,7 @@
 import itertools
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+print=pp.pprint
 
 fields = [
     "grant_msg(n0)", "grant_msg(n1)","grant_msg(n2)","held()",
@@ -35,25 +38,41 @@ eq_class_hex = {
 eq_class_bin = {key: format(val, '0>13b') for key, val in eq_class_hex.items()}
 
 def grant_msg(n, eq_class):
-    return bool(eq_class_bin[eq_class][0 + n])
+    return eq_class_bin[eq_class][0 + n] == "1"
 
 def held(eq_class):
-    return bool(eq_class_bin[eq_class][3])
+    return eq_class_bin[eq_class][3] == "1"
 
 def hold_msg(n, eq_class):
-    return bool(eq_class_bin[eq_class][4 + n])
+    return eq_class_bin[eq_class][4 + n] == "1"
 
 def lock_msg(n, eq_class):
-    return bool(eq_class_bin[eq_class][7 + n])
+    return eq_class_bin[eq_class][7 + n] == "1"
 
 def unlock_msg(n, eq_class):
-    return bool(eq_class_bin[eq_class][10 + n])
+    return eq_class_bin[eq_class][10 + n] == "1"
+
+
 
 num = 3
 state_predicates = [grant_msg, hold_msg, lock_msg, unlock_msg]
+counts = {}
 for L in range(0, 5):
     for subset in itertools.combinations(state_predicates, L):
-        pass
+        count = 0
+        for i in range(num): 
+            a = True
+            for f in subset:
+                # print(f(i, 'A'))
+                a = a and f(i, 'A')
+            if a:
+                count += 1
+        counts[subset] = count
+print(counts)
+
+
+
+        
         
 
 
